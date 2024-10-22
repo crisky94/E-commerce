8db4daa74d71fac9a519185f51fb450fcd05a86e
crisky94 <criistiiniilla.1994@gmail.com>
2024-10-22 15:14:16 +0200

    ruta fail

--- backend/controllers/userController.js
+++
@@ -1,8 +0,0 @@
-// import User from "../models/userModel";
-import asyncHandler from "../middlewares/asyncHandler";
-
-const createUser = asyncHandler(async (req, res) => {
-    res.send('hello');
-});
-
-export {createUser}
