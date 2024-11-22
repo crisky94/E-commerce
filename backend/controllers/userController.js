eb42bf150b4eec323f8c0457edafe59773c5358c
Cris <criistiiniilla.1994@gmail.com>
2024-11-22 12:34:43 +0100

    getCurrentUserProfile, updateCurretnUserProfile

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -37,42 +37,93 @@ const createUser = asyncHandler(async (req, res) => {
 });
 
 const loginUser = asyncHandler(async (req, res) => {
-    const {email, password} = req.body
+    const { email, password } = req.body
 
-    const existingUser = await User.findOne({email})
+    const existingUser = await User.findOne({ email })
 
     if (existingUser) {
         const isPasswordValid = await bcrypt.compare(
-            password, 
+            password,
             existingUser.password)
 
         if (isPasswordValid) {
             createToken(res, existingUser._id)
 
 
-        res.status(201).json({
-            _id: existingUser._id,
-            username: existingUser.username,
-            email: existingUser.email,
-            isAdmin: existingUser.isAdmin,
-        });
+            res.status(201).json({
+                _id: existingUser._id,
+                username: existingUser.username,
+                email: existingUser.email,
+                isAdmin: existingUser.isAdmin,
+            });
             return
         }
     }
 })
 
-const logoutCurrentUser = asyncHandler(async(req, res) =>{
+const logoutCurrentUser = asyncHandler(async (req, res) => {
     res.cookie('jwt', '', {
-        httpOnly:true,
+        httpOnly: true,
         expires: new Date(0),
     });
 
-    res.status(200).json({message: 'Log out successfully'})
+    res.status(200).json({ message: 'Log out successfully' })
 })
 
-const getAllUsers = asyncHandler(async(req, res) => {
+const getAllUsers = asyncHandler(async (req, res) => {
     const users = await User.find({})
     res.json(users)
 })
 
-export { createUser, loginUser, logoutCurrentUser, getAllUsers }
+const getCurrentUserProfile = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id)
+
+    if (user) {
+        res.json({
+            _id: user._id,
+            username: user.username,
+            email: user.email
+        })
+    } else {
+        res.status(404)
+        throw new Error("User not found");
+
+    }
+})
+
+const updateCurretnUserProfile = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id)
+
+    if (user) {
+        user.username = req.body.username || user.username
+        user.email = req.body.email || user.email
+
+        if (req.body.password) {
+            const salt = await bcrypt.genSalt(10)
+            const hashedPassword = await bcrypt.hash(req.body.password, salt)
+            user.password = hashedPassword
+        }
+
+        const updatedUser = await user.save()
+
+        res.json({
+            _id: updatedUser._id,
+            username: updatedUser.username,
+            email: updatedUser.email,
+            isAdmin: updatedUser.isAdmin
+        })
+    } else {
+        res.status(404);
+        throw new Error("User not found");
+
+    }
+})
+
+export {
+    createUser,
+    loginUser,
+    logoutCurrentUser,
+    getAllUsers,
+    getCurrentUserProfile,
+    updateCurretnUserProfile,
+}
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,12 +1,24 @@
 import express from "express";
 const router = express.Router();
-import  {createUser, loginUser, logoutCurrentUser, getAllUsers} from "../controllers/userController.js";
 import { authenticate, authorizedAdmin } from "../middlewares/authMiddleware.js";
+import  {
+    createUser,
+    loginUser, 
+    logoutCurrentUser, 
+    getAllUsers,
+    getCurrentUserProfile,
+    updateCurretnUserProfile,
+} from "../controllers/userController.js";
 
 
 router.route('/').post(createUser).get(authenticate, authorizedAdmin, getAllUsers)
 router.post('/auth', loginUser);
 router.post('/logout', logoutCurrentUser);
+
+router
+    .route('/profile')
+    .get(authenticate, getCurrentUserProfile)
+    .put(authenticate, updateCurretnUserProfile)
 
 
 export default router;
